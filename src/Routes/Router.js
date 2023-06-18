9436842e83f8d935a75b11a93050e6ffcc99aa89
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-06-18 19:38:22 +0600

    added new routes

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -2,6 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../components/Home/Home";
 import Menu from "../components/Menu/Menu";
+import MeatItems from "../FoodsItems/MeatItems/MeatItems";
+import RiceItems from "../FoodsItems/RiceItems/RiceItems";
+import FishItems from "../FoodsItems/FishItems/FishItems";
+import PithaItems from "../FoodsItems/PithaItems/PithaItems";
+import VortaItems from "../FoodsItems/VortaItems/VortaItems";
+import DalAndVaji from "../FoodsItems/DalAndVaji/DalAndVaji";
 
 export const router = createBrowserRouter([
     {
@@ -15,6 +21,30 @@ export const router = createBrowserRouter([
             {
                 path: '/menu',
                 element: <Menu></Menu>
+            },
+            {
+                path: '/meat',
+                element: <MeatItems></MeatItems>
+            },
+            {
+                path: '/rice',
+                element: <RiceItems></RiceItems>
+            },
+            {
+                path: '/fish',
+                element: <FishItems></FishItems>
+            },
+            {
+                path: '/pitha',
+                element: <PithaItems></PithaItems>
+            },
+            {
+                path: '/vorta',
+                element: <VortaItems></VortaItems>
+            },
+            {
+                path: '/dal',
+                element: <DalAndVaji></DalAndVaji>
             }
         ]
     }
